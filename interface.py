6cf7abc9ee5be839a0e6a8082df53cb25c74d8c5
christopherdumas <christopherdumas@me.com>
2016-03-27 20:27:56 -0700

    Finished interface. Going to work on Fuzzy searching now..

--- interface.py
+++ interface.py
@@ -12,9 +12,9 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 
-# TODO: Use Text widget for info_label, instead of Label widget
-# 2) SCroll it.
-# 3) Show suggestions when there are multiple matches for a stock in fuzzy searching.
+# TODO
+# 1) Show suggestions when there are multiple matches for a stock in fuzzy searching.
+# 2) Color the risk level.
 
 
 with open('cboesymboldir2.csv', mode='r') as in_file:
@@ -35,6 +35,9 @@ class App(object):
             
             self.ticker = StringVar(frame)
             self.textbox = Entry(frame, textvariable=self.ticker, width=300)
+            def invest(x):
+                self.invest()
+            self.textbox.bind('<Return>', invest)
 
             self.dayn = IntVar(frame)
             self.day_slider = Scale(frame, from_=1, to=30, variable=self.dayn, orient=HORIZONTAL, length=250)
@@ -44,9 +47,11 @@ class App(object):
             info_card = Frame(frame)
             self.name = StringVar(info_card)
             self.info = StringVar(info_card)
+            self.risk = StringVar(info_card)
 
             self.name_label = Label(info_card, textvariable=self.name, font="Helvetica 32 bold")
-            self.info_label = Label(info_card, textvariable=self.info, font="Helvetica 16", fg="gray")
+            self.risk_label = Label(info_card, textvariable=self.risk, font="Helvetica 16", fg="green")
+            self.info_label = Label(info_card, textvariable=self.info, font="Helvetica 16", fg="darkgray")
 
             scrollbar = Scrollbar(info_card)
             scrollbar.pack(side=RIGHT, fill=Y)
@@ -60,6 +65,7 @@ class App(object):
 
             info_card.pack(side=LEFT, expand=True)
             self.name_label.pack()
+            self.risk_label.pack()
             self.info_label.pack()
             self.info_list.pack(fill=BOTH, expand=True)
             
@@ -98,14 +104,21 @@ class App(object):
                 n, r, b, pi, cpps, ppps = predictions[-1]
                 self.name.set("\n"+ticker_to_name_dict[n].title()
                                 .replace("Stk", "").replace(" Com", "") + " ("+n+") \n")
-                self.info.set("Risk Level: " + r.upper() + "\n" +\
-                                "Current Price per Share: " + str(round(cpps, 2)) + "\n" + \
-                                "Beta: " + str(b) + "\n\n\n")
+                self.risk.set("Risk Level: " + r.upper() + "\n")
+                if r == 'low':
+                    self.risk_label.config(fg="green")
+                elif r == 'medium':
+                    self.risk_label.config(fg="#CCCC00")
+                elif r == 'high':
+                    self.risk_label.config(fg="red")
+                    
+                self.info.set("Current Price per Share: $" + str(round(cpps, 2)) + "\n" + \
+                              "Beta: " + str(b) + "\n\n\n")
 
                 self.info_list.delete(0, END)
                 for n, r, b, pi, cpps, ppps in predictions:
                     self.info_list.insert(END, "Increase: " + str(round(pi*100, 2)) + "%" + ",  " + \
-                                               "Predicted Share Price: " + str(round(ppps, 2)) + "\n\n")
+                                               "Predicted Share Price: $" + str(round(ppps, 2)) + "\n\n")
 
 root = Tk()
 root.geometry('{}x{}'.format(1850, 900))
